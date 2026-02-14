2e87eccc523ec772320753826a688232368cfe78
bulacio nicolas <93942151+nicolasbulacio@users.noreply.github.com>
2026-02-14 14:51:36 -0300

    se conecto de for exitosa a la base de datos

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const conectarDB = require('./config/db'); 
+
+dotenv.config();
+
+conectarDB();
+
+const app = express();
+const puerto = process.env.PORT || 4000;
+
+
+app.use(cors());
+app.use(express.json()); 
+
+
+app.get('/', (req, res) => {
+    res.status(200).json({ 
+        estado: 'Exitoso',
+        mensaje: '¡Servidor del Gimnasio funcionando al 100%!' 
+    });
+});
+
+
+app.listen(puerto, () => {
+    console.log(` Servidor corriendo en el puerto ${puerto}`);
+});
