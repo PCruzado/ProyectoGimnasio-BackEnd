70f668dabf8e986cd61df00cb202b4ccb5f59495
bulacio nicolas <93942151+nicolasbulacio@users.noreply.github.com>
2026-03-04 22:41:55 -0300

    se colocaron los mismos datos que recibe del front

--- models/Product.js
+++ models/Product.js
@@ -6,24 +6,12 @@ const productSchema = new mongoose.Schema({
         required: [true, 'El nombre del producto es obligatorio'],
         trim: true
     },
-    descripcion: {
-        type: String,
-        required: [true, 'La descripción es obligatoria']
-    },
     precio: {
         type: Number,
         required: [true, 'El precio es obligatorio'],
         min: 0
     },
-    categoria: {
-        type: String,
-        required: [true, 'La categoría es obligatoria'],
-        enum: ['Musculación', 'Clases', 'Ropa', 'Suplementos']
-    },
-    stock: {
-        type: Number,
-        default: 0
-    },
+    
     imagen: {
         type: String,
         default: 'https://via.placeholder.com/150'
