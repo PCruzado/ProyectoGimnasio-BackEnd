005a0cd00200df77ba6dffb8c091d028998151d5
Pablito <pablito.cruzadosujeros@gmail.com>
2026-03-04 21:36:20 -0300

    delete/obtenerPerfil

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { registrarUsuario, loginUsuario, obtenerUsuarios, obtenerPerfil, actualizarRolUsuario } = require('../controllers/userController');
+const { registrarUsuario, loginUsuario, obtenerUsuarios, actualizarRolUsuario } = require('../controllers/userController');
 const { protect, admin, superAdmin } = require('../middleware/authMiddleware');
 
 router.post('/registro', registrarUsuario);
